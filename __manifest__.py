18658bdec2a3379fe4951f60fb747c1f5f698949
armannurhidayat <armannurhidayat7@gmail.com>
2020-02-11 23:29:55 +0700

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "printout kanban barcode",
+
+    'summary': """
+        Printout Kanban Barcode
+    """,
+
+    'description': """
+        Printout Kanban Barcode
+    """,
+
+    'author': "Arman Nur Hidayat <Vitraining>",
+    'website': "http://www.vitraining.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','vit_kanban'],
+
+    # always loaded
+    'data': [
+        'report/views.xml',
+    ],
+}
